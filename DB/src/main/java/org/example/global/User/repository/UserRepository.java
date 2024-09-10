a08676d79f7a899d038d8b81491068a11369fc46
ajswl <kkhhjj1209@gmail.com>
2024-09-10 16:57:08 +0900

    delete role

--- DB/src/main/java/org/example/global/User/repository/UserRepository.java
+++ DB/src/main/java/org/example/global/User/repository/UserRepository.java
@@ -1,6 +1,5 @@
 package org.example.global.User.repository;
 
-import org.example.global.User.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User,Integer> {
